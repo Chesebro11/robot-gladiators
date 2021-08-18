5e42ab8ad003b9d135f2f0de31c3689cd2bcde7c
Chesebro11 <84700256+Chesebro11@users.noreply.github.com>
2021-08-18 03:00:32 -0700

    did stuff

---
+++ assets/js/game.js
@@ -0,0 +1,23 @@
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+// You can also log multiple vaules at once like this
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function () {
+    window.alert("Welcome to Robot Gladiators!");
+    enemyHealth = enemyHealth - playerAttack;
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has" + enemyHealth + " health remaining."
+    );
+    playerHealth = playerHealth - enemyAttack;
+    console.log(
+        playerName + " Now has " + playerHealth + " remaining!");
+};
+
+fight();
