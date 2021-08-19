f58e89f35709cbdd748961547ccdb2cea18897e3
Chesebro11 <84700256+Chesebro11@users.noreply.github.com>
2021-08-18 22:04:44 -0700

    added if else statements for player and enemy health values

--- assets/js/game.js
+++ assets/js/game.js
@@ -15,9 +15,23 @@ var fight = function () {
     console.log(
         playerName + " attacked " + enemyName + ". " + enemyName + " now has" + enemyHealth + " health remaining."
     );
+    // check enemy's health
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!");
+    }
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
     playerHealth = playerHealth - enemyAttack;
     console.log(
         playerName + " Now has " + playerHealth + " remaining!");
+    // check player health
+    if (playerHealth <= 0) {
+        window.alert(playerName + " has died!");
+    }
+    else {
+        window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
 };
 
 fight();
