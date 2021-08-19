b7c6c3338e45db1bdeeb67b3a193e3a66ccc966f
Chesebro11 <84700256+Chesebro11@users.noreply.github.com>
2021-08-19 00:23:18 -0700

    fight loop now cycles through each enemy

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,9 @@
+// Game States
+// "win" - Player robot has defeated all enemy-robots
+// * Fight all enemy-robots
+// * Defeat each enemy-robot
+// "lose" - Player robot's health is zero or less
+
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
@@ -6,11 +12,18 @@ var playerMoney = 10;
 // You can also log multiple vaules at once like this
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function () {
+for (var i = 0; i < enemyNames.length; i++) {
+    console.log(enemyNames[i]);
+    console.log(i);
+    console.log(enemyNames[i] + " is at " + i + " index");
+}
+
+var fight = function (enemyName) {
+    // fight function statements
     window.alert("Welcome to Robot Gladiators!");
 
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'Fight' or 'SKIP' to choose");
@@ -59,4 +72,6 @@ var fight = function () {
     }
 };
 
-fight();
+for (var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
